3a1190a9d6df241bc76f63be5acfd6bf5800e682
ronald-bog <105521717+ronald-bog@users.noreply.github.com>
2022-05-22 22:09:41 -0500

    Reto 4 Grupo 46 Universidad Nacional Mintic
    Reto 4 Grupo 46 Universidad Nacional Colombia - Mintic

--- Reto_4.py
+++ Reto_4.py
@@ -1,32 +1,15 @@
 import json
 
 catalogo = json.loads(input("Ingrese Catalogo: "))
-secGenOrd = "".join(sorted(input("Secuencia Genes: ")))
+secGenOrd = sorted(input("Secuencia Genes: "))
 
-valor = 1
-indice = 1
+valor = 0
 genes = ""
-cantidad = ""
-genPersona = {}
-total = 0
-genDis = ""
 
-for i in secGenOrd:
-    if indice != len(secGenOrd) and i == secGenOrd[indice]:
-        valor += 1
-    else:
-        genes += i
-        cantidad += str(valor)
-        genPersona[i] = valor
-        valor = 1
-    indice += 1
+for key, value in catalogo.items():
+    if key in secGenOrd:
+        genes += str(key)
+        valor += (secGenOrd.count(key) * value)
 
-for cat in catalogo:
-    for gp in genPersona:
-        if cat == gp:
-            vrGen = catalogo[cat] * genPersona[gp]
-            total += vrGen
-            genDis += str(cat)
-
-print(total)
-print(genDis)
+print(valor)
+print(genes)
